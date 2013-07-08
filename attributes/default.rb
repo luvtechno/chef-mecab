033ebc41785a96e0d1f95a24ca940981be812e5c
Yoshinori Kawasaki <ykawasaki@gmail.com>
2013-07-09 03:27:49 +0900

    Build and install mecab from source code

--- attributes/default.rb
+++ attributes/default.rb
@@ -24,22 +24,5 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
-mecab_src_url = "https://mecab.googlecode.com/files/mecab-#{node['mecab']['version']}.tar.gz"
-mecab_src_filepath  = "#{Chef::Config['file_cache_path'] || '/tmp'}/mecab-#{node['mecab']['version']}.tar.gz"
-
-remote_file mecab_src_filepath do
-  source mecab_src_url
-  checksum node['mecab']['checksum']
-  backup false
-end
-
-bash "compile_mecab_source" do
-  cwd ::File.dirname(mecab_src_filepath)
-  code <<-EOH
-    tar zxf #{::File.basename(mecab_src_filepath)} -C #{::File.dirname(mecab_src_filepath)} &&
-    cd mecab-#{node['mecab']['version']} &&
-    ./configure &&
-    make &&
-    make install
-  EOH
-end
+default['mecab']['version'] = '0.996'
+default['mecab']['checksum'] = '15baca0983a61c1a49cffd4a919463a0a39ef127'
